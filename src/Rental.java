14e4dedc1d9e314af88479c0b3b058f801e45b08
MH0896 <max.hirte@web.de>
2017-05-09 17:01:40 +0200

    getCharge-Methode in Movie-Klasse verschoben

--- src/Rental.java
+++ src/Rental.java
@@ -17,25 +17,7 @@ class Rental {
     }
     
     private double getCharge() {
-        double res = 0;
-        
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                res += 2;
-                if (getDaysRented() > 2)
-                    res += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                res += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                res += 1.5;
-                if (getDaysRented() > 3)
-                    res += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        
-        return res;
+        return this.movie.getCharge(this.daysRented);
     }
     
     int getFrequentRenterPoints() {
