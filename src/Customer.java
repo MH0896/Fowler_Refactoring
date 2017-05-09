90334b14f58e583d0fc003163dda30e526e810d2
MH0896 <max.hirte@web.de>
2017-05-09 16:38:33 +0200

    Anpassen der Variablen in amountFor-Methode

--- src/Customer.java
+++ src/Customer.java
@@ -53,26 +53,26 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
+    private double amountFor(Rental rent) {
+        double res = 0;
         
         switch (each.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                res += 2;
+                if (rent.getDaysRented() > 2)
+                    res += (rent.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                res += rent.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                res += 1.5;
+                if (rent.getDaysRented() > 3)
+                    res += (rent.getDaysRented() - 3) * 1.5;
                 break;
         }
         
-        return thisAmount;
+        return res;
     }
 }
     
