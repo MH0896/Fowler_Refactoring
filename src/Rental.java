7e4afbdc046f0ac2bd6d3a1c4cf81915de512a85
MH0896 <max.hirte@web.de>
2017-05-09 16:51:15 +0200

    Frequent renter points Ermittlung in Rental-Klasse auslagern

--- src/Rental.java
+++ src/Rental.java
@@ -37,4 +37,13 @@ class Rental {
         
         return res;
     }
+    
+    int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1){
+        	return 2;
+    	}else{
+    	    return 1;
+    	}
+    }
+
 }
